6aded44d7993f660c8261622cf6a479513aceed2
gtf35 <gtfdeyouxiang@gmail.com>
2023-01-29 20:36:13 +0800

    :lipstick: Update navigation bar and status bar utils

--- app/src/main/java/top/nekohelper/wallpaperhelper/utils/ActivityExt.kt
+++ app/src/main/java/top/nekohelper/wallpaperhelper/utils/ActivityExt.kt
@@ -16,6 +16,8 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+@file:Suppress("unused")
+
 package top.nekohelper.wallpaperhelper.utils
 
 import android.app.Activity
@@ -23,21 +25,48 @@ import android.graphics.Color
 import android.os.Build
 import android.view.View
 import android.view.WindowManager
+import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 
 @Suppress("DEPRECATION")
 fun Activity.translateStatusBar() {
     // https://stackoverflow.com/a/68492550
-    if (Build.VERSION.SDK_INT in 21..29) {
+    if (Build.VERSION.SDK_INT in Build.VERSION_CODES.LOLLIPOP .. Build.VERSION_CODES.Q) {
         window.statusBarColor = Color.TRANSPARENT
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.decorView.systemUiVisibility =
             View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 
-    } else if (Build.VERSION.SDK_INT >= 30) {
+    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
         window.statusBarColor = Color.TRANSPARENT
         // Making status bar overlaps with the activity
         WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+}
+
+@Suppress("DEPRECATION")
+fun Activity.translateStatusBarAndNavigationBar() {
+    // https://stackoverflow.com/a/68492550
+    if (Build.VERSION.SDK_INT in Build.VERSION_CODES.LOLLIPOP .. Build.VERSION_CODES.Q) {
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+    } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
+        // Making status bar overlaps with the activity
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+}
+
+fun View.paddingNavBar() {
+    ViewCompat.setOnApplyWindowInsetsListener(this) { _, insets ->
+        val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        setPaddingRelative(paddingStart, paddingTop, paddingEnd, systemBars.bottom)
+        return@setOnApplyWindowInsetsListener insets
     }
 }
